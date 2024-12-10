3abc474b7df41813465226cbb70401b30baf4734
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-12-10 22:17:40 +0300

    scanner manager added

---
+++ src/main/java/com/ofg/utils/ScannerManager.java
@@ -0,0 +1,24 @@
+package com.ofg.utils;
+
+import java.util.Scanner;
+
+public class ScannerManager {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getImagePath() {
+        System.out.print("Enter the absolute path of the image you want to copy: ");
+        return scanner.nextLine();
+    }
+
+    public static String getSquareColor() {
+        System.out.print("Enter the square color: ");
+        return scanner.nextLine();
+    }
+}
+
+//enters image
+//enters color
+//read image
+//resize the image to square
+//save the image
+
